85754e251af24b1c4d85ee839d845c5f8551f1b7
timanrebel <timanrebel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-28 22:11:54 +0000

    now really fixt the bug
    git-svn-id: https://plugins.svn.wordpress.org/rss-link-tagger-for-google-analytics/tags/1.1.2@150156 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ rss-link-tagger-for-google-analytics.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: RSS Link Tagger for Google Analytics
+Plugin URI: http://wordpress.rebelic.nl/rsslinktagger
+Description: Modifies RSS permalinks to include utm query parameters, used by Google Analytics to track non-adwords advertising campaigns.
+Version: 1.1.2
+Author: Timan Rebel
+Author URI: http://wordpress.rebelic.nl/
+*/
+
+function rsslinktagger($guid) {
+	global $wp;
+
+	if (strpos($wp->request, 'feed') !== false) {
+
+		$delimiter = '?';
+
+		if (strpos ( $guid, $delimiter ) > 0)
+			$delimiter = '&amp;';
+
+		return $guid . $delimiter . 'utm_source=rss&amp;utm_medium=rss&amp;utm_campaign=rss';
+	}
+	return $guid;
+}
+
+//add_filter ( 'get_the_guid', 'rsslinktagger' );
+add_filter ( 'the_permalink_rss', 'rsslinktagger' );
+
+?>
