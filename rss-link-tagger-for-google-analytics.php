e58f079280bb28f14ffd810a7b03d4242aa8e8e5
timanrebel <timanrebel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-28 10:22:26 +0000

    fixt bug handling feed/atom and feed/rdf requests
    git-svn-id: https://plugins.svn.wordpress.org/rss-link-tagger-for-google-analytics/trunk@149956 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rss-link-tagger-for-google-analytics.php
+++ rss-link-tagger-for-google-analytics.php
@@ -1,34 +1,17 @@
 <?php
 /*
 Plugin Name: RSS Link Tagger for Google Analytics
-Plugin URI: http://trebel.nl/blog/rsslinktagger
+Plugin URI: http://wordpress.rebelic.nl/rsslinktagger
 Description: Modifies RSS permalinks to include utm query parameters, used by Google Analytics to track non-adwords advertising campaigns.
-Version: 1.1
+Version: 1.1.1
 Author: Timan Rebel
-Author URI: http://trebel.nl/
-*/
-
-/*  Copyright 2008 Timan Rebel  (email : wordpress@trebel.nl)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+Author URI: http://wordpress.rebelic.nl/
 */
 
 function rsslinktagger($guid) {
 	global $wp;
 
-	if ($wp->request == 'feed') {
+	if (strpos($wp->request, 'feed') !== false) {
 
 		$delimiter = '?';
 
